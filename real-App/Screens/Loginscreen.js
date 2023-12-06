38d5a508bfc425e85d23ebd187d2d61dfbfde121
Ntampul <ntampulamthunzi@gmail.com>
2023-12-06 11:59:29 +0200

    fingerprints

--- real-App/Screens/Loginscreen.js
+++ real-App/Screens/Loginscreen.js
@@ -1,14 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { KeyboardAvoidingView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Icon from 'react-native-vector-icons/FontAwesome5'; // You can replace 'FontAwesome5' with the appropriate icon set
 
-const loginscreen = () => {
+const Loginscreen = () => {
   return (
-    <View>
-      <Text>loginscreen</Text>
+
+      <View style={{ flex: 1,  alignItems: 'center', backgroundColor:"#101E32"}}>
+    <Icon name="fingerprint" size={50} color="#7D8243" /> 
+
+    <Text style={{color: "white", marginBottom: 20 }}> Sign in with touch ID</Text>
+
+      <KeyboardAvoidingView>
+    
+      </KeyboardAvoidingView>
     </View>
+    
   )
 }
 
-export default loginscreen
+export default Loginscreen
 
 const styles = StyleSheet.create({})
--- real-App/Stacknavigator.js
+++ real-App/Stacknavigator.js
@@ -1,21 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import loginscreen from './Screens/Loginscreen';
-import RegisterSreen from './Screens/RegisterSreen';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Loginscreen from "./Screens/Loginscreen";
+import RegisterSreen from "./Screens/RegisterSreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 const stacknavigator = () => {
-    const Stack = createNativeStackNavigator();
+  const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={loginscreen} option= {{ hearderShown: false}}/>
-      <Stack.Screen name="Register" component={RegisterSreen} option= {{ hearderShown: false}} />
-    </Stack.Navigator>
-  </NavigationContainer>
-  )
-}
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Loginscreen}
+          options={{ hearderShown: false }}
+        />
+        <Stack.Screen
+          name="Register"
+          component={RegisterSreen}
+          options={{ hearderShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
-export default stacknavigator
+export default stacknavigator;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
