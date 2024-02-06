ad1d2c2238d75df6730ff500c57ff9ff1c241c3e
Ntampul <ntampulamthunzi@gmail.com>
2024-02-06 15:17:10 +0200

    reg

--- real-App/Screens/RegisterSreen.js
+++ real-App/Screens/RegisterSreen.js
@@ -1,23 +1,44 @@
 import { StyleSheet, Text, View, TextInput, KeyboardAvoidingView, Pressable} from 'react-native'
 import React, { useState } from 'react'
+import { useNavigation } from "@react-navigation/native";
 
 const RegisterSreen = () => {
   const [email,setEmail]= useState("");
   const [name,setName]= useState("");
   const [password,setPassword]= useState("");
   const [image,setImage]= useState("");
+  const navigation = useNavigation();
   return (
     <View style={{ flex: 1, alignItems: "center", backgroundColor: "#101E32" }}>
       <KeyboardAvoidingView>
         <View style={{ flex: 1, alignItems: "center", marginTop: 150 }}>
           <Text style={{ color: "white", marginBottom: 10, fontWeight: 800 }}>
             {" "}
-            Sign in
+            Register
           </Text>
           <Text style={{ color: "white", marginBottom: 35, fontWeight: 100 }}>
             {" "}
-            Sign in to your account{" "}
+            Register To Your Account{" "}
           </Text>
+
+          <View>
+            <Text style={{ fontSize: 18, fontWeight: "600", color: "white" }}>
+              Name
+            </Text>
+            <TextInput
+              value={password}
+              onChangeText={(text) => setPassword(text)}
+              style={{
+                fontSize: password ? 18 : 18,
+                borderBottomColor: "gray",
+                borderBottomWidth: 1,
+                marginVertical: 10,
+                width: 300
+              }}
+              placeholderTextColor={"gray"}
+              placeholder="Enter Your Name"
+            />
+          </View>
           
           <View>
             <Text style={{ fontSize: 18, fontWeight: "600", color: "white" }}>
@@ -45,6 +66,27 @@ const RegisterSreen = () => {
             </Text>
 
             <TextInput
+              value={name}
+              onChangeText={(text) => setName(text)}
+              style={{
+                fontSize: password ? 18 : 18,
+                borderBottomColor: "gray",
+                borderBottomWidth: 1,
+                marginVertical: 10,
+                width: 300
+              }}
+              placeholderTextColor={"gray"}
+              placeholder="Password"
+            />
+          </View>
+
+          
+          <View>
+            <Text style={{ fontSize: 18, fontWeight: "600", color: "white" }}>
+              Image
+            </Text>
+
+            <TextInput
               value={password}
               onChangeText={(text) => setPassword(text)}
               style={{
@@ -55,15 +97,15 @@ const RegisterSreen = () => {
                 width: 300,marginBottom: 50
               }}
               placeholderTextColor={"gray"}
-              placeholder="Enter Your Password"
+              placeholder="Image"
             />
           </View>
         <Pressable  style={{ width: 150, backgroundColor: "gray",  marginRight: "auto", marginLeft: "auto", padding:15, borderRadius: 30,}}>
-          <Text style={{ color:"white",fontSize: 16, fontWeight:"bold", textAlign: "center",}}> LogIn </Text>
+          <Text style={{ color:"white",fontSize: 16, fontWeight:"bold", textAlign: "center",}}> Register</Text>
         </Pressable>
 
          <Pressable >
-          <Text  onPress={() => navigation.navigate("Register")} style={{color: "white", marginBottom: 35, fontWeight: 100,marginBottom: 10}}>Don't have an account? Sign Up</Text>
+          <Text  onPress={() => navigation.goBack()} style={{color: "white", marginBottom: 35, fontWeight: 100,marginBottom: 10}}>Already have an account? Sign in</Text>
          </Pressable>
 
         </View>
